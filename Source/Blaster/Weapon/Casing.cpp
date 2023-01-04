5c8741ea7a82bcde2e084adb0182cf943e6befcd
Jimmy <jkchuong@gmail.com>
2023-01-04 20:37:44 +0000

    added object glow and physics to bullet casing

--- Source/Blaster/Weapon/Casing.cpp
+++ Source/Blaster/Weapon/Casing.cpp
@@ -3,17 +3,44 @@
 
 #include "Casing.h"
 
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
+
 ACasing::ACasing()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
 	CasingMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CasingMesh"));
 	SetRootComponent(CasingMesh);
+
+	// So the player camera snap in when moving over a casing
+	CasingMesh->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
+	
+	CasingMesh->SetSimulatePhysics(true);
+	CasingMesh->SetNotifyRigidBodyCollision(true); // To generate hit events with simulate physics on
+	CasingMesh->SetEnableGravity(true);
+	ShellEjectionImpulse = 10.0f;
 }
 
 void ACasing::BeginPlay()
 {
 	Super::BeginPlay();
+
+	SetLifeSpan(5);
 	
+	CasingMesh->OnComponentHit.AddDynamic(this, &ACasing::OnHit);
+	CasingMesh->AddImpulse(GetActorForwardVector() * ShellEjectionImpulse);
+}
+
+void ACasing::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	FVector NormalImpulse, const FHitResult& Hit)
+{
+	// To stop the sound cue from playing more than once
+	CasingMesh->SetNotifyRigidBodyCollision(false);
+	
+	if (ShellSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, ShellSound, GetActorLocation());
+	}
 }
 
